d35f5e65e824b9582baa6d7c28570945e89c78a7
Ywintersss <shawnpvp@gmail.com>
2024-07-31 21:58:04 +0800

    layout + routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Layout from './components/layout'
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      // parent route wraps child routes
+      element: <Layout />,
+      children : 
+      [
+        // Outlet is used to render child route components when the corresponding url is matched.
+        { 
+          path: "/",
+          element: ""
+        },
+      ]
+    }
+  ])
+
+
   return (
     <div className="App">
-      
+      {/* RouterProvider is used to render child routes */}
+      <RouterProvider router={router} />
     </div>
   );
 }
