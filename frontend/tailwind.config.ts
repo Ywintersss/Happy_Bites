c1091aeffd0c6e868b69328b2b11e91fd9ae15dd
Ywintersss <shawnpvp@gmail.com>
2024-10-05 18:20:12 +0800

    New Initialization

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,46 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      colors: {
+        'primary': {
+          'light': '',
+          'DEFAULT': '',
+          'dark': ''
+        },
+        'secondary': {
+          'light': '',
+          'DEFAULT': '',
+          'dark': ''
+        },
+        'accent': {
+          'light': '',
+          'DEFAULT': '',
+          'dark': ''
+        }
+      }
+    },
+  },
+  plugins: [],
+};
+
+export default config;
